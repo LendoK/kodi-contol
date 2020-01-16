e9a1ec597695b8bbed74e7deed5c3223bc8958dd
lendo <keilbaum@web.de>
2020-01-16 18:17:23 +0100

    add normal m3u8 streams

--- media_sources/generic_video.js
+++ media_sources/generic_video.js
@@ -1,6 +1,6 @@
 var video = function(url){
     // var match = /\.(mp4|mkv|mov|avi|flv|wmv|asf|mp3|flac|mka|m4a)/.exec(url);
-    var match = /(\.(mp4|mkv|mov|avi|flv|wmv|asf|mp3|flac|mka|m4a))|(master(_(\d+))*\.m3u8)/.exec(url);
+    var match = /(\.(mp4|mkv|mov|avi|flv|wmv|asf|mp3|flac|mka|m4a|m3u8))|(master(_(\d+))*\.m3u8)/.exec(url);
     if (match) {
         var media = {};
         var filename = url.replace(/^.*[\\\/]/, '');
